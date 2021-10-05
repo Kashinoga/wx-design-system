a4d45df8c2d2e5f2144393d81cdea973384e3c66
Kashinoga <45315255+Kashinoga@users.noreply.github.com>
2021-10-04 20:01:16 -0500

    Mythic card work

--- src/routes/mythic.svelte
+++ src/routes/mythic.svelte
@@ -18,6 +18,70 @@
 	<title>About | WX Design System</title>
 </svelte:head>
 
-<div class="content">Test</div>
+<div class="content">
+	<h1>Cards</h1>
+	<div class="deck">
+		<div class="card">
+			<div class="card-header">
+				<div class="card-header-block-left">
+					R
+				</div>
+				<div class="card-header-text">
+					Card
+				</div>
+			</div>
+			<div class="card-content">Content</div>
+			<div class="card-footer">
+				<div class="card-footer-text">Footer</div>
+			</div>
+		</div>
+	</div>
+</div>
 
-<style></style>
+<style>
+	@media only screen and (max-width: 425px) {
+		.deck {
+			align-items: center;
+			display: flex;
+			flex-direction: column;
+		}
+	}
+
+	.card {
+		border: 1px solid black;
+		border-radius: 0.3em;
+		display: flex;
+		flex-direction: column;
+		height: 300px;
+		width: 200px;
+	}
+
+	.card-header {
+		border-bottom: 1px dashed black;
+		display: flex;
+		flex-direction: row;
+
+	}
+
+	.card-header-block-left {
+		border-right: 1px dashed black;
+		padding: 0.5em;
+	}
+
+	.card-header-text {
+		padding: 0.5em;
+	}
+
+	.card-content {
+		padding: 1em;
+	}
+
+	.card-footer {
+		border-top: 1px dashed black;
+		display: flex;
+		font-size: 0.8em;
+		justify-content: flex-end;
+		margin-top: auto;
+		padding: 0.2em;
+	}
+</style>
